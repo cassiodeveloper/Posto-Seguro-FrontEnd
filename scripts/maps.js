bf20d98ed64e4fbda13a01c2057473d718602b62
Cássio B. Pereira <cassio@cassiobp.com.br>
2016-09-26 17:47:10 -0300

    Markers e Info Window

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,13 +1,16 @@
-var domain = 'http://postoseguro.azurewebsites.net';
+var domain = 'http://localhost:52821';
 var api = '/api/Posto';
+var defaultLat = -23.5655317;
+var defaultLng = -46.6546707;
+var defaultZomm = 10;
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -23.6815315, lng: -46.8754831},
+        center: {lat: defaultLat, lng: defaultLng},
         scrollwheel: true,
         zoomControl: true,
         scaleControl: true,       
-        zoom: 15,
+        zoom: defaultZomm,
         mapTypeControl: true,
         mapTypeControlOptions: {
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
@@ -18,23 +21,7 @@ function initMap() {
         }
     });
 
-    //var infowindow = new google.maps.InfoWindow();
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-       
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            map.setCenter(pos);
-        }, function() {
-            //handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        //handleLocationError(false, infoWindow, map.getCenter());
-    }
+    getGeolocation(map);
 
     getPostos(map);
 }
@@ -53,6 +40,59 @@ function getPostos(map) {
                 map: map,
                 title: posto.Nome
             });
+
+            marker.addListener('click', function() {
+                setInfoWindowContent(posto.Id, map, marker);
+            });
         });
     });
+}
+
+function setInfoWindowContent(postoId, map, marker) {
+
+    $.getJSON(domain + api, function(jsonResponse) {
+        $.each(jsonResponse, function(key, posto) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: configureInfoWindowLayout(posto)
+            });
+
+            infoWindow.open(map, marker);
+        });
+    });
+}
+
+function configureInfoWindowLayout(posto) {
+    return  '<div id="content">' +
+                '<div id="siteNotice"></div>' +
+                '<h1 id="firstHeading" class="firstHeading">' + posto.Nome + '</h1>' +
+                '<div id="bodyContent">' +
+                    '<p>Bandeira: <b>' + posto.Bandeira + '</b></p>' +
+                    '<p>Tipo: <b>' + posto.Tipo + '</b></p>' +
+                    '<p>Endereço: ' + posto.Endereco + ', ' + posto.Cidade + ', ' + posto.Bairro + ', ' + posto.Estado +'</p>' +                    
+                    '<p>Penalidades: <ul>' + posto.Penalidades.forEach(function(penalidade) {
+                        '<li>' + penalidade.Data + '</li>'
+                        '<li>' + penalidade.Descricao + '</li>'
+                        '<li>' + penalidade.Tipo + '</li>'
+                    }) +
+                    '</ul></p>' +
+                '</div>' +
+            '</div>';
+}
+
+function getGeolocation(map) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+       
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            map.setCenter(pos);
+        }, function() {
+            //handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        //handleLocationError(false, infoWindow, map.getCenter());
+    }    
 }
